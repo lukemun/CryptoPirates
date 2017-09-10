b5386c0b1437ff95b10656a317986eaecf898a57
Alex Matthys <alexmatthys@gmail.com>
2017-09-09 21:35:47 -0400

    creating program design

--- getPrice.py
+++ getPrice.py
@@ -1,13 +1,9 @@
+
+# alex matthys testing 
+
+
 import threading
 import requests as req
-
-def getPrice(url, payload):
-	threading.Timer(5, getPrice, (url, payload)).start()
-	response_json = req.get(url, params=payload)
-	data = response_json.text
-	prices.append(data)
-	print (prices)
-
 
 btc = "BTC"
 usd = "USD"
@@ -16,7 +12,24 @@ url = "https://min-api.cryptocompare.com/data/price"
 
 prices = []
 
+def getPrice(url, payload):
+	threading.Timer(5, getPrice, (url, payload)).start()
+	response_json = req.get(url, params=payload)
+	data = response_json.text
+	prices.append(data)
+
+	# Analyze data, decide if we need a buy or sell
+	analyze(prices)
+
+	print (prices)
+
+
+# Function to decide given a list of bitcoin prices whether we should buy, sell, or hold
+def analyze(prices):
+	pass
+
+
 getPrice(url, payload)
 
 
-# alex testing 
+
