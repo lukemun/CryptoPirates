99ff6fb8958ccb1d08c85b6b2e7303676f7a29aa
Alex Matthys <alexmatthys@gmail.com>
2017-09-09 21:49:54 -0400

    added a deque data structure

--- getPrice.py
+++ getPrice.py
@@ -4,19 +4,22 @@
 
 import threading
 import requests as req
+import collections as c
 
 btc = "BTC"
 usd = "USD"
 payload = {"fsym": btc, "tsyms": usd}
 url = "https://min-api.cryptocompare.com/data/price"
 
-prices = []
+prices = c.deque()
 
 def getPrice(url, payload):
-	threading.Timer(5, getPrice, (url, payload)).start()
+	threading.Timer(10, getPrice, (url, payload)).start()
 	response_json = req.get(url, params=payload)
 	data = response_json.text
 	prices.append(data)
+	if len(prices) > 3600:
+		prices.popleft()
 
 	# Analyze data, decide if we need a buy or sell
 	analyze(prices)
