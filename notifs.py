e4078c0f5f1e478b4682d4f58d45bbb5b9135ca6
Luke <lmunro@calpoly.edu>
2017-11-01 16:46:25 -0700

    poloniex

--- notifs.py
+++ notifs.py
@@ -6,4 +6,4 @@ def send_mail(msg, emails):
 	server.login("tifmrp1324ip@gmail.com", "wussgood$$$")
 	for email in emails:
 		server.sendmail("tifmrp1324ip@gmail.com", email, msg)
-	server.quit() 
+	server.quit()
