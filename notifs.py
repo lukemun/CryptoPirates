db1caa3a3a116b8586767d65b079a87d85ca3381
lukemun <lmunro@usc.edu>
2018-01-08 13:06:06 -0800

    cleaning

--- notifs.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-def send_mail(msg, emails):
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
-	server.login("tifmrp1324ip@gmail.com", "wussgood$$$")
-	for email in emails:
-		server.sendmail("tifmrp1324ip@gmail.com", email, msg)
-	server.quit()
